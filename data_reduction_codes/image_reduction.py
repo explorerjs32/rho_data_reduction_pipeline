25211f44e6bd669280530740a0cdea673aef4823
stefanocandiani1 <scandiani22500@columbushs.com>
2024-07-13 15:29:32 -0400

    Master_Bias Update - (07/13/24)

--- data_reduction_codes/image_reduction.py
+++ data_reduction_codes/image_reduction.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from astropy.io import fits
-import os 
+import os
 import argparse
 
 
@@ -10,14 +10,17 @@ def get_frame_info(data_dir, file_list):
     """
     Extracts information from FITS file headers and returns it as pandas DataFrames.
 
+
     This function reads the FITS files located in the specified directory, extracts relevant
-    information from the headers (object name, frame type, exposure time, and filter), and 
-    compiles this information into a pandas DataFrame. Additionally, it generates an observing 
+    information from the headers (object name, frame type, exposure time, and filter), and
+    compiles this information into a pandas DataFrame. Additionally, it generates an observing
     log DataFrame based on the extracted frame information.
+
 
     Parameters:
     data_dir (str): The directory where the FITS files are stored.
     file_list (list of str): A list of FITS file names to be processed.
+
 
     Returns:
     tuple:
@@ -31,6 +34,16 @@ def get_frame_info(data_dir, file_list):
                           with a column 'Exposures' indicating the number of exposures for each group.
     """
 
+    ''' NOTE: inside this function, getheader uses [data_dir][file], which assumes that data_dir (args.data) has
+        "/" at the end of it, like "data/2024-04-15/" rather than "data/2024-04-15". If the latter is entered
+        as a command line argument, the program will search for files in the data folder like "data/2024-04-15rho..."
+        instead of "data/2024-04-15/rho..." which obviously does not exist and results in an error.
+
+        This is just something to keep in mind when running it and later on if we work on a way to automate running
+        this program. Optionally, we could have a function that will add the "/" at the end of the string if it is
+        missing, but for now it's not a big deal.
+    '''
+
     # Define the lists to store the data
     exposure_times = []
     filters = []
@@ -39,7 +52,6 @@ def get_frame_info(data_dir, file_list):
 
     # Loop through the light frames to get the information out of the fits file header
     for file in file_list:
-
         # Get the object name
         obj_name = fits.getheader(f"{data_dir}{file}")['OBJECT']
         objects.append(obj_name)
@@ -48,7 +60,7 @@ def get_frame_info(data_dir, file_list):
         frame = fits.getheader(f"{data_dir}{file}")['FRAME']
         frames.append(frame)
 
-        # Get the exposure time 
+        # Get the exposure time
         exp_time = fits.getheader(f"{data_dir}{file}")['EXPTIME']
         exposure_times.append(exp_time)
 
@@ -57,16 +69,54 @@ def get_frame_info(data_dir, file_list):
         filters.append(filter)
 
     # Generate a dataframe containing the frame information
-    frame_info_df = pd.DataFrame({'Files':file_list,
-                                'Object':objects,
-                                'Frame':frames, 
-                                'Filter':filters, 
-                                'Exptime':exposure_times})
-    
+    frame_info_df = pd.DataFrame({'Files': file_list,
+                                  'Object': objects,
+                                  'Frame': frames,
+                                  'Filter': filters,
+                                  'Exptime': exposure_times})
+
     # Generate the observing log based on the frame information
-    observing_log_df = frame_info_df.groupby(by=['Object','Frame','Filter','Exptime']).size().to_frame(name='Exposures').reset_index()
+    observing_log_df = frame_info_df.groupby(by=['Object', 'Frame', 'Filter', 'Exptime']).size().to_frame(
+        name='Exposures').reset_index()
 
     return frame_info_df, observing_log_df
+
+
+def create_master_darks(frame_info_df):
+    """
+    Creates a list of master darks from the information in the two dataframes.
+
+    First, isolates the dark frames and how many unique exposures there are, then iterates through
+    the list of darks for each exposure time to gather the dark frames for a specific exposure time, which
+    it will median combine into a master dark.
+
+    Args:
+        frame_info_df: the frame information dataframe
+        observing_log_df: the dataframe list of unique frame types
+
+
+    Returns:
+        dark_exposure_times: a list of master dark exposure times (float) that correlate to the master darks
+        master_darks: a dictionary of master darks. Each object in the list is fits data.
+            Key: "master_darks_[0.0]s" where [0.0] is replaced with the exposure time
+            Value: fits data (2D array of pixel counts)
+    """
+    # creating the master darks- one for each exposure time.]
+    # for each unique exposure (entry in observing log that is a dark frame), get that exposure time
+    darks_df = frame_info_df[frame_info_df['Frame'] == 'Dark'].reset_index(drop=True)
+    dark_exposure_times = darks_df['Exptime'].unique()
+
+    # go through the darks of that exposure length to create the master-
+    master_darks = {}
+    for exp in dark_exposure_times:
+        darks_exp = []
+        for index, row in darks_df.iterrows():
+            if (row["Exptime"] == exp):
+                darks_exp.append(fits.getdata(f"{args.data}{row['Files']}"))
+        master_darks["master_dark_" + str(exp) + "s"] = np.median(np.array(darks_exp), axis=0)
+
+    # return the darks and the times they correlate to.
+    return dark_exposure_times, master_darks
 
 
 def create_master_bias(frame_info_df, data_dir):
@@ -103,19 +153,23 @@ def create_master_bias(frame_info_df, data_dir):
 
 
 # Define the arguments to parse into the script
-parser = argparse.ArgumentParser(description="Arguments to parse for the data reduction pipeline. Primarily foccusing on the directories where the data is stored.")
+parser = argparse.ArgumentParser(
+    description="Arguments to parse for the data reduction pipeline. Primarily foccusing on the directories where the data is stored.")
 
 parser.add_argument('-D', '--data', type=str, required=True, help="Directory where the collected data is stored.")
 parser.add_argument('-b', '-bias_frames', type=str, default='', help="Directory where the bias frames are stored.")
-parser.add_argument('-d','--dark_frames', type=str, default='', help="Directory where the dark frames are stored.")
-parser.add_argument('-f','--flat_frames', type=str, default='', help="Directory where the flat frames are stored.")
-parser.add_argument('-l', '--light_frames', type=str, default='', help="Directory where the light (science) frames are stored.")
+parser.add_argument('-d', '--dark_frames', type=str, default='', help="Directory where the dark frames are stored.")
+parser.add_argument('-f', '--flat_frames', type=str, default='', help="Directory where the flat frames are stored.")
+parser.add_argument('-l', '--light_frames', type=str, default='',
+                    help="Directory where the light (science) frames are stored.")
 
 args = parser.parse_args()
 
-
 # Extract the frame information from the collected data and the observing log
 frame_info_df, observing_log_df = get_frame_info(args.data, os.listdir(args.data))
+
+# create the master darks
+dark_times, master_darks = create_master_darks(frame_info_df)
 
 # Identify master bias frames and combine them
 master_bias = create_master_bias(frame_info_df, args.data)
